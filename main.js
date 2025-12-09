f2e24f33d230d6e578de420359da7173b555319a
Tempter03 <sholokhov69505new@gmail.com>
2025-12-09 14:52:20 +0500

    newPromo

--- main.js
+++ main.js
@@ -183,7 +183,13 @@ function finishGame(winner) {
 }
 
 function generatePromo() {
-  return Math.floor(10000 + Math.random() * 90000).toString();
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+  for (let i = 0; i < 5; i += 1) {
+    const idx = Math.floor(Math.random() * chars.length);
+    code += chars[idx];
+  }
+  return code;
 }
 
 function resetGame() {
